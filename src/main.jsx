c300ab3adf343b06a4fa7b73f1ddfd059c41beaa
Manikandan-RR <rajmani1985py@gmail.com>
2025-06-27 16:54:55 +0530

    Router Issue

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-    <BrowserRouter>
+    <BrowserRouter basename="/REACT_PORTFOLIO" >
       <App />
     </BrowserRouter>
-    
+
   </StrictMode>,
 )
