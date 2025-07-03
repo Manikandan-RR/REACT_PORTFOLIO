071462c07683c9bba434d859c9e99f6ee48efb08
Manikandan-RR <rajmani1985py@gmail.com>
2025-07-03 19:41:59 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-   base: '/REACT_PORTFOLIO/',
+export default defineConfig({   
     plugins: [react()],
 
 })
